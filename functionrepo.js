a958266ad7eda7f05825fb3db77a8686b92eb499
javariyafatima1 <hoorfatima279@gmail.com>
2023-11-10 21:16:22 +0500

    mam repo function practice

---
+++ functionrepo.js
@@ -0,0 +1,76 @@
+// // Q1.==>  Write a program that returns your age in days. (take age as an input)
+
+// function ageInDays(a){
+//         var age = a * 365
+//         return age;
+    
+//     }
+//     var age = +prompt('Enter your age')
+//     var ageInDay = ageInDays(age)
+//     console.log(`I am ${age} years${ageInDay} days .`)
+
+
+    // // Q2.==> Create a function that takes a number as an argument, increments the number by +1 and returns the result. (take number as an input)
+
+    // function increments(b) {
+    //         return ++b;
+    //     }
+        
+    //     var result = increments(+prompt(""))
+    //     console.log( result)
+
+//  Q3.===> Create a function that takes a number as an argument, decrements the number by -1 and returns the result.
+
+// function deccrement(a) {
+//     return --a;
+// }
+
+// var result = deccrement(prompt(""))
+// console.log( result)
+//  Q4. ===> Create a function that takes a number and return square of a number
+
+// function squareNumber(number) {
+//     return number * number;
+//   }
+//   var squared = squareNumber(prompt(""));
+//   console.log(squared);
+
+
+//  Q5.===> Create a function that tells current time in Y-m-d format.
+
+
+// function getCurrentTime() {
+//     const now = new Date();
+  
+//     const year = now.getFullYear();
+//     const month = String(now.getMonth() + 1)
+//     const day = String(now.getDate())
+  
+//     const currentTime = `${year}-${month}-${day}`;
+  
+//     return currentTime;
+//   }
+
+//   const currentTime = getCurrentTime();
+//   console.log(currentTime);
+
+
+//  Q6. ===> Create a function  that tells current time from Date object in H:i:s format.
+// function getCurrentTime() {
+//     const now = new Date();
+  
+
+//     const hours = now.getHours()
+//     const minutes = now.getMinutes()
+//     const seconds = now.getSeconds()
+  
+   
+//     const currentTime = `${hours}:${minutes}:${seconds}`;
+  
+//     return currentTime;
+//   }
+// const currentTime = getCurrentTime();
+//   console.log(currentTime);
+
+
+
